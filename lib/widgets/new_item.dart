345d55a517bf3fa1470564249ba8a5797d802f21
Anuj-k-45 <anujkaushal1068@gmail.com>
2024-04-12 22:05:21 +0530

    Using firebase as backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'
+    as http; //this http could be anyname and it is just a name of an object in which the bundle is saved
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
@@ -15,17 +18,48 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = "";
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!
           .save(); //when this line exicutes a special function will be exicuted in all the textfield areas and that is onSaved function
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https("flutter-prep-277c9-default-rtdb.firebaseio.com",
+          "shopping-list.json");
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(
+          {
+            "name": _enteredName,
+            "quantity": _enteredQuantity,
+            "category": _selectedCategory.title,
+          },
+        ),
+      );
+
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      print(response
+          .body); //you will see an output like "name:xyz123" here this name is not the name of the item. dont get confused, this "name" is the name given by firebase to the response
+      print(
+          response.statusCode); //status code returns the status of the request
+
+      if (!context.mounted) {
+        //while check weather the current context is the same as the previos one before awaiting or not (for example if there was a situation where some data was to be deleted and the request and response was not yet completed then would have been wrong if you got directly forward and run the code with the incomplete context)
+        return;
+      }
+
       Navigator.of(context).pop(
+        //very very very important to understand, when you pop this screen and navigate back to the last screen this will return back a value and there will be the stored in the variable(if used) which was assigned the value while navigating to this screen
         GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
+          id: responseData["name"],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
       );
     }
   }
@@ -125,14 +159,22 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text("Reset"),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: const Text("Add Item"),
+                    onPressed: _isSending ? null : _saveItem,
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text("Add Item"),
                   ),
                 ],
               ),
